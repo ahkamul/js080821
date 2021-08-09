5d50ce84992382a9beff0b8d2b751fc09e686880
ahkamul <sasraishougat@gmail.com>
2021-08-09 11:50:27 +0600

    new file index.js created at aug 08, 21 and pushing it to js080821 repo at aug 09, 21

---
+++ index.js
@@ -0,0 +1,52 @@
+// Class 044: JS Array Traversing 
+
+// Ex. 1 
+
+// var arr = [4,5,1,6,8,9,40]
+
+// for (var i = 0; i < arr.length; i++){
+//     console.log(arr[i])
+// }
+
+
+// Ex. 2
+
+// var arr = [4,5,1,6,8,9,40]
+
+// for (var i = 0; i < arr.length; i++){
+//     arr[i] = arr[i] + 2
+// }
+
+// console.log(arr)
+
+
+// Ex. 3 
+
+// var sum = 0
+// var arr = [4,5,1,6,8,9,40]
+// for (var i = 0; i < arr.length; i ++){
+//     sum = sum + arr[i]
+// }
+// console.log(sum)
+
+
+// Ex. 4
+
+// var arr = [4,5,1,6,8,9,40]
+
+// for (var i = 0; i < arr.length; i ++){
+//     if(arr[i] % 2 === 0){
+//         console.log(arr[i])
+//     }
+// }
+
+
+// Ex. 5
+
+var arr = [4,5,1,6,8,9,40]
+
+for (var i = 0; i < arr.length; i ++){
+    if(arr[i] % 2 === 1){
+        console.log(arr[i])
+    }
+}
